264a2f17511f4843c2e796f764a5a89e42aae944
Bruno Santos <bruno_st17@hotmail.com>
2024-03-10 18:23:21 -0300

    feat: ajustando

--- main.tf
+++ main.tf
@@ -13,19 +13,11 @@ resource "aws_subnet" "supera_subnet1" {
   vpc_id            = aws_vpc.opentofu_supera_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
   tags = {
     Name = "subnet_1_supera"
   }
 }
-
-#resource "aws_subnet" "supera_subnet2" {
-#  vpc_id            = aws_vpc.opentofu_supera_vpc.id
-#  cidr_block        = "10.0.2.0/24"
-#  availability_zone = "us-east-1a"
-#  tags = {
-#    Name = "subnet_2_supera"
-#  }
-#}
 
 resource "aws_security_group" "opentofu_supera_sg" {
   name        = "opentofu_supera_sg"
@@ -58,9 +50,3 @@ output "instance_ip" {
   value = aws_instance.supera_opentofu_instance1.public_ip
 }
 
-#resource "aws_instance" "supera_opentofu_instance2" {
-#  ami                    = "ami-0f403e3180720dd7e"
-#  instance_type          = "t2.micro"
-#  subnet_id              = aws_subnet.supera_subnet2.id
-#  vpc_security_group_ids = [aws_security_group.opentofu_supera_sg.id]
-#}
