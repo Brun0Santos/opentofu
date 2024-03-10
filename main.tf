62fa58e31a21fb3d4b2c1c296039511cf6807562
Bruno Santos <bruno_st17@hotmail.com>
2024-03-10 11:43:07 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "opentofu_supera_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "supera_subnet1" {
+  vpc_id            = aws_vpc.opentofu_supera_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "supera_subnet2" {
+  vpc_id            = aws_vpc.opentofu_supera_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_instance" "supera_opentofu_instance1" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.supera_subnet1.id
+}
+
+resource "aws_instance" "supera_opentofu_instance2" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.supera_subnet2.id
+}
---
+++ src/main/java/com/opentofu/controller/OpenTofuController.java
@@ -0,0 +1,17 @@
+package com.opentofu.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class OpenTofuController {
+    private static Integer requestCount = 0;
+
+    @GetMapping
+    public String message() {
+        requestCount += 1;
+        return "Server is running, number of requests: " + requestCount;
+    }
+}
