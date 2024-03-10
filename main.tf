debbf2a01d2c99ad40a801f7952a0318ad0a3d76
Bruno Santos <bruno_st17@hotmail.com>
2024-03-10 19:58:58 -0300

    test

--- main.tf
+++ main.tf
@@ -19,6 +19,32 @@ resource "aws_subnet" "supera_subnet1" {
   }
 }
 
+resource "aws_internet_gateway" "opentofu_supera_igw" {
+  vpc_id = aws_vpc.opentofu_supera_vpc.id
+
+  tags = {
+    Name = "opentofu_supera_igw"
+  }
+}
+
+resource "aws_route_table" "opentofu_supera_rt" {
+  vpc_id = aws_vpc.opentofu_supera_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.opentofu_supera_igw.id
+  }
+
+  tags = {
+    Name = "opentofu_supera_rt"
+  }
+}
+
+resource "aws_route_table_association" "opentofu_supera_rta" {
+  subnet_id      = aws_subnet.supera_subnet1.id
+  route_table_id = aws_route_table.opentofu_supera_rt.id
+}
+
 resource "aws_security_group" "opentofu_supera_sg" {
   name        = "opentofu_supera_sg"
   description = "Security group for OpenTofu instances"
@@ -43,7 +69,6 @@ resource "aws_instance" "supera_opentofu_instance1" {
   ami                    = "ami-0f403e3180720dd7e"
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.supera_subnet1.id
-  key_name               = "chave-ssh-opentofu"
   vpc_security_group_ids = [aws_security_group.opentofu_supera_sg.id]
   tags = {
     Name = "instance_tofu_supera"
@@ -53,4 +78,3 @@ resource "aws_instance" "supera_opentofu_instance1" {
 output "instance_ip" {
   value = aws_instance.supera_opentofu_instance1.public_ip
 }
-
