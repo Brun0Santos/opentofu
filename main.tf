3996028a4db2d5862d5881323727e27972ead1ed
Bruno Santos <bruno_st17@hotmail.com>
2024-03-10 12:04:47 -0300

    fix: corrigindo alguns problemas

--- main.tf
+++ main.tf
@@ -18,14 +18,36 @@ resource "aws_subnet" "supera_subnet2" {
   availability_zone = "us-east-1a"
 }
 
+resource "aws_security_group" "opentofu_supera_sg" {
+  name        = "opentofu_supera_sg"
+  description = "Security group for OpenTofu instances"
+  vpc_id      = aws_vpc.opentofu_supera_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "supera_opentofu_instance1" {
-  ami           = "ami-0f403e3180720dd7e"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.supera_subnet1.id
+  ami                    = "ami-0f403e3180720dd7e"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.supera_subnet1.id
+  vpc_security_group_ids = [aws_security_group.opentofu_supera_sg.id]
 }
 
 resource "aws_instance" "supera_opentofu_instance2" {
-  ami           = "ami-0f403e3180720dd7e"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.supera_subnet2.id
+  ami                    = "ami-0f403e3180720dd7e"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.supera_subnet2.id
+  vpc_security_group_ids = [aws_security_group.opentofu_supera_sg.id]
 }
